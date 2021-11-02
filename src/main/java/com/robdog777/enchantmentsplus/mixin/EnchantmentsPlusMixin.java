35d8d0411506ae2c0ac422ab8fb4efbf644605b4
Robert Houeland <houelanr@oregonstate.edu>
2021-11-01 21:17:59 -0700

    update to 1.17.1

--- src/main/java/com/robdog777/enchantmentsplus/mixin/EnchantmentsPlusMixin.java
+++ src/main/java/com/robdog777/enchantmentsplus/mixin/EnchantmentsPlusMixin.java
@@ -5,11 +5,21 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mixin(TitleScreen.class)
 public class EnchantmentsPlusMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        System.out.println("This line is printed by an example mod mixin!");
+    private static final Logger LOGGER = LogManager.getLogger("enchantmentsplus");
+
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        LOGGER.info("Enchantments plus is running");
     }
+
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {}
 }
--- src/main/java/com/robdog777/enchantmentsplus/mixin/PlayerEntityMixin.java
+++ src/main/java/com/robdog777/enchantmentsplus/mixin/PlayerEntityMixin.java
@@ -1,6 +1,10 @@
 package com.robdog777.enchantmentsplus.mixin;
 
 import com.robdog777.enchantmentsplus.enchants.NightVisionEnchantment;
+import com.robdog777.enchantmentsplus.enchants.BlazeWalkerEnchantment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
@@ -8,14 +12,22 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Map;
+
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
+
+    private Object ItemStackHelper;
 
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
@@ -26,11 +38,22 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             at = @At("HEAD")
     )
     private void tick(CallbackInfo ci) {
-        Item helmet = this.getEquippedStack(EquipmentSlot.HEAD).getItem();
+        Item stack = this.getEquippedStack(EquipmentSlot.HEAD).getItem();
 
-        if(helmet.hasNightVision) {
+        /*if(EnchantmentHelper.getLevel(Enchantment NightVisionEnchantment, ItemStack stack)) {
             this.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION,
                     1000000, 0));
-        }
+        }*/
+    }
+
+    protected void applyMovementEffects(BlockPos pos, CallbackInfo ci) {
+        /*LivingEntity casted = (LivingEntity) (Object) this;
+        Map.Entry<EquipmentSlot, ItemStack> entry = EnchantmentHelper.chooseEquipmentWith(BlazeWalkerEnchantment, casted);
+
+        if (entry != null) {
+            ItemStack item = (ItemStack) ((Map.Entry<?, ?>) entry).getValue();
+            BlazeWalkerEnchantment enchantment = ((BlazeWalkerEnchantment) (BlazeWalkerEnchantment));
+            enchantment.freezeLava(casted, this.world, pos, level, item), item);
+        }*/
     }
 }
