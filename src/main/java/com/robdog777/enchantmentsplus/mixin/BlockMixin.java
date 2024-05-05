5d323bb506a2ba1ea68f953ba66067c076483c95
Robert Houeland <71792920+TheRobdog@users.noreply.github.com>
2024-05-04 18:24:57 -0700

    fix: flash forge works correctly and drops XP for smelting ores

--- src/main/java/com/robdog777/enchantmentsplus/mixin/BlockMixin.java
+++ src/main/java/com/robdog777/enchantmentsplus/mixin/BlockMixin.java
@@ -3,6 +3,7 @@ package com.robdog777.enchantmentsplus.mixin;
 import com.robdog777.enchantmentsplus.EnchantmentsPlus;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
@@ -19,7 +20,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,35 +27,40 @@ import java.util.Optional;
 public class BlockMixin {
     // smelt with FlashForge
     @Inject(method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;",
-            at = @At("RETURN"), cancellable = true)
-    private static void getDroppedStacks(BlockState state, ServerWorld world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack, CallbackInfoReturnable<List<ItemStack>> cir) {
-        List<ItemStack> items = new ArrayList<>();
+            at = @At("RETURN"))
+    private static void getDroppedStacks(BlockState state, ServerWorld world, BlockPos pos, BlockEntity blockEntity,
+                                         Entity entity, ItemStack stack, CallbackInfoReturnable<List<ItemStack>> cir) {
         List<ItemStack> returnValue = cir.getReturnValue();
 
         // Flash Forge
         if (EnchantmentHelper.getLevel(EnchantmentsPlus.FLASHFORGE, stack) == 0
                 || !EnchantmentsPlus.CONFIG_HOLDER.getConfig().enableFlashForge) {
-            cir.setReturnValue(returnValue);
             return;
         }
-        for (ItemStack itemStack : returnValue) {
+
+        for (int i = 0; i < returnValue.size(); i++) {
+            ItemStack itemStack = returnValue.get(i);
             Optional<RecipeEntry<SmeltingRecipe>> recipe = world.getRecipeManager()
                     .listAllOfType(RecipeType.SMELTING).stream()
-                    .filter((smeltingRecipe -> smeltingRecipe.value().getIngredients().get(0).test(itemStack))).findFirst();
+                    .filter(r -> r.value().getIngredients().get(0).test(itemStack)).findFirst();
+
             if (recipe.isPresent()) {
                 DynamicRegistryManager registryManager = world.getRegistryManager();
                 ItemStack smelted = recipe.get().value().getResult(registryManager);
                 smelted.setCount(itemStack.getCount());
-                items.add(smelted);
+                returnValue.set(i, smelted);
 
                 // Spawn XP orbs
                 int xpAmount = (int) recipe.get().value().getExperience();
                 world.spawnEntity(new ExperienceOrbEntity(world, pos.getX(), pos.getY(), pos.getZ(), xpAmount));
-            } else {
-                items.add(itemStack);
+
+                if (state.getBlock() == Blocks.IRON_ORE || state.getBlock() == Blocks.DEEPSLATE_IRON_ORE ||
+                        state.getBlock() == Blocks.COPPER_ORE || state.getBlock() == Blocks.DEEPSLATE_COPPER_ORE) {
+                    // these ores are hardcoded to drop XP since they work differently
+                    // technically smelting ore gives 0.7 XP but we're rounding for an int
+                    world.spawnEntity(new ExperienceOrbEntity(world, pos.getX(), pos.getY(), pos.getZ(), 1));
+                }
             }
         }
-
-        cir.setReturnValue(items);
     }
 }
