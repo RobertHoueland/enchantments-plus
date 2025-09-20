332e22b5ce6d965c5a12f50c8510ba4e5a7d7382
Robert Houeland <71792920+TheRobdog@users.noreply.github.com>
2025-09-20 14:41:22 -0700

    optimize tick calls

--- src/main/java/com/robdog777/enchantmentsplus/mixin/LivingEntityMixin.java
+++ src/main/java/com/robdog777/enchantmentsplus/mixin/LivingEntityMixin.java
@@ -2,7 +2,9 @@ package com.robdog777.enchantmentsplus.mixin;
 
 import com.robdog777.enchantmentsplus.EnchantmentsPlus;
 import com.robdog777.enchantmentsplus.SharedStates;
+import com.robdog777.enchantmentsplus.config.EnchantmentsPlusConfig;
 import com.robdog777.enchantmentsplus.enchants.BlazeWalkerEnchantment;
+import me.shedaniel.autoconfig.ConfigHolder;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -59,48 +61,63 @@ public abstract class LivingEntityMixin extends Entity {
     protected void tick(CallbackInfo ci) {
         LivingEntity currentEntity = (LivingEntity) (Object) this;
 
-        // Lunar Sight
-        int nightVisionLevel = EnchantmentHelper.getEquipmentLevel(EnchantmentsPlus.LUNARSIGHT, currentEntity);
-        if (nightVisionLevel > 0 && EnchantmentsPlus.CONFIG_HOLDER.getConfig().enableLunarSight) {
-            // Stays for 11 seconds, otherwise sky flashes at <=10 seconds
-            currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION,
-                    220, 0, false, false, true));
-        }
-
-        // Moon Walker
-        int moonWalkerLevel = EnchantmentHelper.getEquipmentLevel(EnchantmentsPlus.MOONWALKER, currentEntity);
-        if (moonWalkerLevel > 0 && EnchantmentsPlus.CONFIG_HOLDER.getConfig().enableMoonWalker) {
-            // constant effects
-            currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20,
-                    0, false, false, true));
-            currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20,
-                    0, false, false, true));
-
-            // cool down for stronger effect
-            RegistryEntry<StatusEffect> moonrestEntry = Registries.STATUS_EFFECT.getEntry(EnchantmentsPlus.MOONREST);
-            if (!currentEntity.hasStatusEffect(moonrestEntry)) {
-                // TODO: add keybind
-                currentEntity.getWorld().playSound(null, currentEntity.getBlockPos(), EnchantmentsPlus.SwoopEvent,
-                        currentEntity.getSoundCategory(), 1.0f, 1f);
-                currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, moonWalkerLevel * 100,
-                        moonWalkerLevel + 1, false, false, true));
-
-                int moonRestCooldown = 400; // 20 seconds
-                currentEntity.addStatusEffect(new StatusEffectInstance(moonrestEntry,
-                        moonRestCooldown, 0, false, false, true));
-            }
-        }
-
         // Dual Leap sound effects
+        // TODO: play on servers
         if (currentEntity instanceof PlayerEntity player) {
             if (SharedStates.dualLeapSuccessful) {
                 player.getWorld().playSound(null, player.getBlockPos(), EnchantmentsPlus.WhooshEvent,
                         SoundCategory.PLAYERS, 0.7f, 1f);
+
                 SharedStates.dualLeapSuccessful = false;
             } else if (SharedStates.dualLeapFailed) {
                 player.getWorld().playSound(null, player.getBlockPos(), EnchantmentsPlus.DenyEvent,
                         SoundCategory.PLAYERS, 0.7f, 1f);
+
                 SharedStates.dualLeapFailed = false;
+            }
+        }
+
+        // Only process every 5 ticks (4 times per second) instead of every tick
+        if (currentEntity.age % 5 == 0) {
+            ConfigHolder<EnchantmentsPlusConfig> config = EnchantmentsPlus.CONFIG_HOLDER;
+
+            // Lunar Sight
+            if (config.getConfig().enableLunarSight) {
+                int nightVisionLevel = EnchantmentHelper.getEquipmentLevel(EnchantmentsPlus.LUNARSIGHT, currentEntity);
+
+                if (nightVisionLevel > 0) {
+                    // Stays for 11 seconds, otherwise sky flashes at <=10 seconds
+                    currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION,
+                            220, 0, false, false, true));
+                }
+            }
+
+            // Moon Walker
+            if (config.getConfig().enableMoonWalker) {
+                RegistryEntry<StatusEffect> MOONREST_ENTRY = Registries.STATUS_EFFECT.getEntry(EnchantmentsPlus.MOONREST);
+                int moonWalkerLevel = EnchantmentHelper.getEquipmentLevel(EnchantmentsPlus.MOONWALKER, currentEntity);
+
+                if (moonWalkerLevel > 0) {
+                    // constant effects
+                    currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 20,
+                            0, false, false, true));
+
+                    currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 20,
+                            0, false, false, true));
+
+                    // cool down for stronger effect
+                    if (!currentEntity.hasStatusEffect(MOONREST_ENTRY)) {
+                        // TODO: add keybind
+                        currentEntity.getWorld().playSound(null, currentEntity.getBlockPos(),
+                                EnchantmentsPlus.SwoopEvent, currentEntity.getSoundCategory(), 1.0f, 1f);
+
+                        currentEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST,
+                                moonWalkerLevel * 100, moonWalkerLevel + 1, false, false, true));
+
+                        currentEntity.addStatusEffect(new StatusEffectInstance(MOONREST_ENTRY,
+                                400, 0, false, false, true));
+                    }
+                }
             }
         }
     }
